5a27485dc08ef4b936f63e550570002e2bbee20e
Danielv123 <tranen.bananen@gmail.com>
2018-03-05 19:18:00 +0100

    Moved config to seperate directory

---
+++ config/config.js
@@ -0,0 +1,10 @@
+module.exports = {
+	token: '2d51b016-04bd-47c1-a04b-24671f8cdd16',
+	memorySegment: 'stats',
+	shard: 'shard2',
+	scanInterval: 15000,
+	webserver: {
+		port: 8081,
+	},
+	prometheusPrefix: "screeps_",
+}
--- index.js
+++ index.js
@@ -1,16 +1,7 @@
 const fs = require("fs");
 const {ScreepsAPI} = require("screeps-api");
 
-const config = {
-	token: '2d51b016-04bd-47c1-a04b-24671f8cdd16',
-	memorySegment: 'stats',
-	shard: 'shard2',
-	scanInterval: 15000,
-	webserver: {
-		port: 8081,
-	},
-	prometheusPrefix: "screeps_",
-}
+const config = require("config/config.js");
 
 const http = require("http");
 let server = http.createServer((req,res) => {
